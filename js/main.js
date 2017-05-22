62b59fa415d50e09adaeb67370471f008f1a1b69
JuliaAf <a290288@gmail.com>
2017-05-22 10:43:05 +0400

    clients changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 $(document).ready(function(){
-  $('.speaker__slider').bxSlider({
-    // mode:'fade',
-    auto: true,
-    speed:1000,
-    pause:8000,
-    controls:true
-  });
-});
+	/*slider*/
+	$('.speaker__slider').bxSlider({
+		mode:"fade",
+		auto: true,
+		speed:1000,
+		pause:5000,
+		controls:true
+	});
+
+
+/*toggle hint*/
+ $(".scheme__hint-close").click(function(){
+		if($(window).width()>991){
+			$(".scheme__hint").fadeOut();
+			}
+	});
+	$(".scheme__img").click(function(){
+		 if($(window).width()>991){
+			 $(".scheme__hint").fadeIn();
+			 }
+	 });
+
+
+/*anchor scroll*/
+	$(".nav").on("click","a", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr("href"),
+					top = $(id).offset().top;
+			$("body,html").animate({scrollTop: top}, 500);
+			return false;
+	});
+
+});//ready
